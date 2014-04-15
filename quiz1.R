efb082904f4ae2a544c550dbf25c20becbd5039d
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-04-15 21:53:42 +0200

    quiz1 finish

--- quiz1.R
+++ quiz1.R
@@ -26,3 +26,19 @@ y <- if(x < 3) {
 } else {
   10
 }
+
+
+
+  h <- function(x, y = NULL, d = 3L) {
+    z <- cbind(x, d)
+    if(!is.null(y))
+      z <- z + y
+    else
+      z <- z + f
+    g <- x + y / z
+    if(d == 3L)
+      return(g)
+    g <- g + 10
+    g
+  }
+
