28ce9af23f3e818a2ab911092bab4ea03a006e45
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-04-16 17:32:27 +0200

    copy and rename mean functions to solve functions

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = numeric()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
