74f68d7f381abbc238865df4ee324096cd3aabb1
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-05-03 13:35:57 +0200

    finish R and getdata courses

--- getdata-week4-videos.R
+++ getdata-week4-videos.R
@@ -36,3 +36,50 @@ sub("_","",names(reviews),)
 testName<-"this_is_a_test"
 sub("_","",testName)
 gsub("_","",testName)
+
+grep("Alameda",cameraData$intersection)
+table(grepl("Alameda",cameraData$intersection))
+cameraData2<-cameraData[!grepl("Alameda",cameraData$intersection),]
+
+grep("Alameda",cameraData$intersection,value=TRUE)
+grep("JeffStreet",cameraData$intersection)
+length(grep("JeffStreet",cameraData$intersection))
+
+library(stringr)
+nchar("Jeffrey Leek")
+substr("Jeffrey Leek",1,7)
+paste("Jeffrey","Leek")
+paste0("Jeffrey","Leek")
+str_trim("Jeff     ")
+
+# Date
+d1=date()
+d1
+class(d1)
+d2=Sys.Date()
+d2
+class(d2)
+format(d2,"%a %b %d")
+x=c("1jan1960","2jan1960")
+z=as.Date(x,"%d%b%Y")
+z
+z[1]-z[2]
+as.numeric(z[1]-z[2])
+
+weekdays(d2)
+months(d2)
+julian(d2)
+
+install.packages("lubridate")
+library(lubridate)
+ymd("20140108")
+mdy("08/04/2013")
+dmy("03-04-2013")
+
+ymd_hms("2011-08-03 10:15:03")
+ymd_hms("2011-08-03 10:15:03",tz="Pacific/Auckland")
+?Sys.timezone
+
+x=dmy(c("1jan1960","2jan1960"))
+wday(x[1])
+wday(x[1],label=TRUE)
