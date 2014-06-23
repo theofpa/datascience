abfc8731318e6410acdc9e7774ddb166a376d5f6
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-06-23 22:53:49 +0200

    myapp update

--- myapp/ui.R
+++ myapp/ui.R
@@ -5,18 +5,17 @@ shinyUI(
   pageWithSidebar(
     # Application title
     headerPanel("Diabetes prediction"),
-  
+    
     sidebarPanel(
       numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
       submitButton('Submit')
     ),
     mainPanel(
-        h3('Results of prediction'),
-        h4('You entered'),
-        verbatimTextOutput("inputValue"),
-        h4('Which resulted in a prediction of '),
-        verbatimTextOutput("prediction")
+      h3('Results of prediction'),
+      h4('You entered'),
+      verbatimTextOutput("inputValue"),
+      h4('Which resulted in a prediction of '),
+      verbatimTextOutput("prediction")
     )
   )
 )
-
